ffd1fd61ca6847d10248d50c43afd31f9ae3f394
bangyen <bangyen@northwestern.edu>
2023-11-11 01:00:38 -0600

    added contracts and fixed bugs

--- minimization/utilities.rkt
+++ minimization/utilities.rkt
@@ -1,24 +1,42 @@
 #lang rosette
 
-(provide (all-defined-out))
+(provide
+       (contract-out
+           [struct matrix
+                   ((len  natural?)
+                    (data data?))]
+           [split  (or/c (->* (matrix?
+                               (-> row? boolean?))
+                              (#:ir? boolean?)
+                              (values (or/c matrix? data?)
+                                      (or/c matrix? data?))))]
+           [redundant? (-> matrix?
+                           (-> row? boolean?))]
+           [essential? (-> matrix?
+                           natural?
+                           (-> row? boolean?))]
+           [adequate?  (-> matrix?
+                           matrix?
+                           boolean?)]))
 
-(define nat?
-    exact-nonnegative-integer?)
+(define row?  (cons/c natural? bv?))
+(define data? (listof row?))
 
-(struct matrix (num data)
-    #:guard (lambda (num data name)
-                (unless (nat? num)
+(struct matrix (len data)
+    #:guard (lambda (len data name)
+                (unless (natural? len)
                     (error "invalid width"))
-                (unless ((listof (cons/c (and/c nat? (<=/c num))
-                                         (bitvector num)))
+                (unless ((listof (cons/c (and/c natural?
+                                                (<=/c len))
+                                         (bitvector len)))
                         data)
                     (error "invalid data"))
 
-                (values num data)))
+                (values len data)))
 
-(define (split mat pred [ir? #f])
+(define (split mat pred #:ir? [ir? #f])
     (define data  (matrix-data mat))
-    (define len   (matrix-num  mat))
+    (define len   (matrix-len  mat))
 
     (define true  (filter     pred data))
     (define false (filter-not pred data))
@@ -28,29 +46,34 @@
 
 (define (redundant? mat)
     (lambda (vec)
-        (ormap (lambda (v)
-                       (if (>= (car v) (car vec))
-                           (bveq (bvor (cdr v)
-                                       (cdr vec))
-                                 (cdr v))
-                           #f))
-               (matrix-data mat))))
+        (> (foldl (lambda (v res)
+                          (if (>= (car v) (car vec))
+                              (if (bveq (bvor (cdr v)
+                                              (cdr vec))
+                                        (cdr v))
+                                  (add1 res)
+                                  res)
+                              res))
+                  0 (matrix-data mat))
+           1)))
 
 (define (essential? mat num)
-    (define len  (matrix-num  mat))
     (define data (matrix-data mat))
+    (define len  (matrix-len  mat))
+    (define one  (bv 1 len))
 
     (define pow (build-list len
                             (lambda (n)
-                                    (bvshl (bv 1 len)
+                                    (bvshl one
                                            (bv n len)))))
 
-    (define (count vec)
-        (foldl (lambda (v res)
-                       (if (bvzero? (bvand v vec))
+    (define (count v2)
+        (foldl (lambda (v1 res)
+                       (if (bvzero? (bvand v1 v2))
                            res (add1 res)))
                0
-               data))
+               (map cdr
+                    data)))
 
     (define criteria
         (foldl (lambda (v res)
@@ -61,12 +84,14 @@
                pow))
 
     (lambda (v)
-            (not (bvzero? (bvand v criteria)))))
+            (not (bvzero? (bvand (cdr v)
+                                 criteria)))))
 
 (define (adequate? new old)
     (define (all mat)
-        (bvor (map cdr
-                   (matrix-data mat))))
+        (apply bvor
+               (map cdr
+                    (matrix-data mat))))
 
     (bveq (all new)
           (all old)))
