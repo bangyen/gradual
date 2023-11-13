9b347081fbfbe8cd0add8bbdd69c9cc8c32c7c08
bangyen <bangyen@northwestern.edu>
2023-11-12 23:52:04 -0600

    added greedy tests and refactored code

--- minimization/test.rkt
+++ minimization/test.rkt
@@ -1,27 +1,33 @@
 #lang rosette
 
 (require "utilities.rkt"
+         "algorithms.rkt"
          rackunit)
 
 (define/contract (matrix-eq? m1 m2)
     (-> matrix? matrix? boolean?)
-    (match-let ([(matrix len1 data1) m1]
-                [(matrix len2 data2) m2])
-        (if (= len1 len2)
-            (andmap (lambda (p1 p2)
-                            (match-let ([(cons n1 v1) p1]
-                                        [(cons n2 v2) p2])
-                                (if (= n1 n2)
-                                    (bveq v1 v2)
-                                    #f)))
-                    data1
-                    data2)
-            #f)))
+    (match-define (matrix data1 len1) m1)
+    (match-define (matrix data2 len2) m2)
+
+    (define (row-eq? r1 r2)
+        (match-define (cons n1 v1) r1)
+        (match-define (cons n2 v2) r2)
+
+        (and (= n1 n2)
+             (bveq v1 v2)))
+
+    (if (and (= len1 len2)
+             (= (length data1)
+                (length data2)))
+        (andmap row-eq?
+                data1
+                data2)
+        #f))
 
 (define/contract (count num)
     (-> natural? natural?)
-    (let-values ([(q r)
-                  (quotient/remainder num 2)])
+    (let*-values ([(q/r) quotient/remainder] 
+                  [(q r) (q/r num 2)])
         (if (zero? num)
             0 (+ (count q) r))))
 
@@ -29,11 +35,12 @@
     (->* (natural?) ()
          #:rest (listof natural?)
          matrix?)
-    (matrix len
-            (map (lambda (v)
-                         (cons (count v)
-                               (bv v len)))
-                 data)))
+    (define (row num)
+        (cons (count num)
+              (bv num len)))
+
+    (matrix (map row data)
+            len))
 
 (define/contract (gen . data)
     (->* () ()
@@ -43,24 +50,47 @@
            (cons 2 data)))
 
 
-(let* ([mat  (gen   #b11 #b01)]
-       [pred (redundant?  mat)]
-       [data (matrix-data mat)])
+(test-case
+    "greedy tests"
+    (define dub (gen #b11 #b11 #b01))
+    (define two (gen      #b11 #b01))
+    (define one (gen           #b11))
+
+    (define m1 (scar (greedy dub)))
+    (define m2 (scar (greedy two)))
+    (define m3 (scar (greedy one)))
+
+    (check-true (matrix-eq? m1 one))
+    (check-true (matrix-eq? m2 one))
+    (check-true (matrix-eq? m3 one)))
+
+(test-case
+    "redundant?/divide tests"
+    (define mat  (gen   #b11 #b01))
+    (define pred (redundant?  mat))
+    (define data (matrix-data mat))
+    (define spl  (divide pred mat))
+
     (check-false (pred (car  data)))
     (check-true  (pred (cadr data)))
 
-    (let-values ([(r nr) (split mat pred)])
-        (check-true (matrix-eq? r  (gen #b01)))
-        (check-true (matrix-eq? nr (gen #b11)))))
+    (check-true (matrix-eq? (scar spl) (gen #b01)))
+    (check-true (matrix-eq? (scdr spl) (gen #b11))))
 
-(let* ([mat  (gen #b11 #b01)]
-       [pred (essential?  mat 1)]
-       [data (matrix-data mat)])
+(test-case
+    "essential? tests"
+    (define mat  (gen #b11 #b01))
+    (define pred (essential?  mat 1))
+    (define data (matrix-data mat))
+
     (check-true  (pred (car   data)))
     (check-false (pred (cadr  data))))
 
-(let ([three (gen #b11 #b10 #b01)]
-      [two   (gen      #b10 #b01)]
-      [one   (gen           #b01)])
+(test-case
+    "adequate? tests"
+    (define three (gen #b11 #b10 #b01))
+    (define two   (gen      #b10 #b01))
+    (define one   (gen           #b01))
+
     (check-true  (adequate? two three))
     (check-false (adequate? one two)))
