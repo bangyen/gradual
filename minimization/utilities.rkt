8be4160d87bfddba83cd6a9655dd877e81c97c16
bangyen <bangyen@northwestern.edu>
2023-11-12 23:53:07 -0600

    added split struct

--- minimization/utilities.rkt
+++ minimization/utilities.rkt
@@ -1,98 +1,153 @@
 #lang rosette
 
 (provide
-       (contract-out
-           [struct matrix
-                   ((len  natural?)
-                    (data data?))]
-           [split  (or/c (->* (matrix?
-                               (-> row? boolean?))
-                              (#:ir? boolean?)
-                              (values (or/c matrix? data?)
-                                      (or/c matrix? data?))))]
-           [redundant? (-> matrix?
-                           (-> row? boolean?))]
-           [essential? (-> matrix?
-                           natural?
-                           (-> row? boolean?))]
-           [adequate?  (-> matrix?
-                           matrix?
-                           boolean?)]))
+    (contract-out
+        [row?   contract?]
+        [data?  contract?]
+        [struct matrix
+                ((data data?)
+                 (len  natural?))]
+        [struct split
+                ((in  data?)
+                 (out data?)
+                 (len natural?))]
+        [scar   (-> split? matrix?)]
+        [scdr   (-> split? matrix?)]
+        [divide (->i ([pred (-> row? boolean?)]
+                      [mat  (or/c matrix? data?)])
+                     ([num  (mat)
+                            (if (matrix? mat)
+                                false?
+                                natural?)])
+                     [result split?])]
+        [redundant? (-> matrix?
+                        (-> row? boolean?))]
+        [essential? (-> matrix?
+                        natural?
+                        (-> row? boolean?))]
+        [adequate?  (-> matrix?
+                        matrix?
+                        boolean?)]))
 
 (define row?  (cons/c natural? bv?))
 (define data? (listof row?))
 
-(struct matrix (len data)
-    #:guard (lambda (len data name)
+(define/contract (valid? len)
+    (-> natural? contract?)
+    (listof
+        (cons/c
+            (and/c natural?
+                   (<=/c len))
+            (bitvector len))))
+
+(struct matrix (data len)
+    #:guard (lambda (data len name)
                 (unless (natural? len)
                     (error "invalid width"))
-                (unless ((listof (cons/c (and/c natural?
-                                                (<=/c len))
-                                         (bitvector len)))
-                        data)
+                (unless ((valid? len) data)
                     (error "invalid data"))
 
-                (values len data)))
+                (values data len)))
 
-(define (split mat pred #:ir? [ir? #f])
-    (define data  (matrix-data mat))
-    (define len   (matrix-len  mat))
+(struct split (in out len)
+    #:guard (lambda (in out len name)
+                (unless (natural? len)
+                    (error "invalid width"))
+                (unless ((valid? len) in)
+                    (error "invalid in data"))
+                (unless ((valid? len) out)
+                    (error "invalid out data"))
+
+                (values in out len)))
+
+(define (scar spl)
+    (matrix (split-in  spl)
+            (split-len spl)))
+
+(define (scdr spl)
+    (matrix (split-out spl)
+            (split-len spl)))
+
+(define (divide pred mat [num #f])
+    (define bool (matrix? mat))
+
+    (define data
+        (if bool
+            (matrix-data mat)
+            mat))
+
+    (define len
+        (if bool
+            (matrix-len mat)
+            num))
 
     (define true  (filter     pred data))
     (define false (filter-not pred data))
 
-    (values (if ir? true  (matrix len true))
-            (if ir? false (matrix len false))))
+    (split true false len))
 
 (define (redundant? mat)
-    (lambda (vec)
-        (> (foldl (lambda (v res)
-                          (if (>= (car v) (car vec))
-                              (if (bveq (bvor (cdr v)
-                                              (cdr vec))
-                                        (cdr v))
-                                  (add1 res)
-                                  res)
-                              res))
-                  0 (matrix-data mat))
-           1)))
+    (define (pred vec)
+        (define (count v res)
+            (if (and (< res 2)
+                     (>= (car v) (car vec))
+                     (bveq (bvor (cdr v)
+                                 (cdr vec))
+                           (cdr v)))
+                (add1 res)
+                res))
+
+        (define data (matrix-data mat))
+        (define num  (foldl count 0 data))
+
+        (> num 1))
+
+    pred)
 
 (define (essential? mat num)
     (define data (matrix-data mat))
     (define len  (matrix-len  mat))
-    (define one  (bv 1 len))
 
-    (define pow (build-list len
-                            (lambda (n)
-                                    (bvshl one
-                                           (bv n len)))))
+    (define (shift n)
+        (bvshl (bv 1 len)
+               (bv n len)))
 
     (define (count v2)
-        (foldl (lambda (v1 res)
-                       (if (bvzero? (bvand v1 v2))
-                           res (add1 res)))
-               0
-               (map cdr
-                    data)))
+        (define (diff v1 res)
+            (if (bvzero? (bvand v1 v2))
+                res (add1 res)))
+
+        (foldl diff 0
+               (map cdr data)))
+
+    (define (build v res)
+        (if (= (count v) num)
+            (bvor v res) res))
+
+    (define (pred v)
+        (define val
+            (bvand (cdr v)
+                   criteria))
+
+        (not (bvzero? val)))
 
     (define criteria
-        (foldl (lambda (v res)
-                       (if (= (count v) num)
-                           (bvor v res)
-                           res))
+        (foldl build
                (bv 0 len)
-               pow))
+               (build-list len
+                           shift)))
 
-    (lambda (v)
-            (not (bvzero? (bvand (cdr v)
-                                 criteria)))))
+    pred)
 
 (define (adequate? new old)
     (define (all mat)
+        (define data (matrix-data mat))
+        (define len  (matrix-len  old))
+
         (apply bvor
-               (map cdr
-                    (matrix-data mat))))
+               (cons (bv 0 len)
+                     (map cdr
+                          data))))
 
     (bveq (all new)
           (all old)))
-
