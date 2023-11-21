1246031e135f2f503cad8b0ad4ab748e67cc0675
bangyen <bangyen@northwestern.edu>
2023-11-20 18:43:38 -0600

    added folder, moved example to separate file

---
+++ minimization/pipeline/tests.rkt
@@ -0,0 +1,28 @@
+#lang rosette
+
+(require "mutate.rkt"
+         mutate/mutators/code
+         mutate)
+
+(define engine
+    (build-mutation-engine
+        #:mutators
+        replace-constants/similar
+        #:syntax-only
+        #:streaming))
+
+(define program
+    #'(define (fact n)
+          (if (> n 0)
+              (* n (fact (sub1 n)))
+              1)))
+
+(define tests
+    (list
+        #'(check-eq? (fact 0)  1)
+        #'(check-eq? (fact 1)  1)
+        #'(check-eq? (fact 2)  2)
+        #'(check-eq? (fact 3)  6)
+        #'(check-eq? (fact 4) 24)))
+
+(pipeline engine program tests)
